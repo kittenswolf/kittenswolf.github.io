e40f328f3289afacf961080d5ed07dbd6f92a262
kittenswolf <kittenswolf@users.noreply.github.com>
2018-08-09 19:51:01 +0200

    test

--- js/stars.js
+++ js/stars.js
@@ -20,9 +20,44 @@ function draw_star(cx, cy, spikes, outerRadius, innerRadius) {
     ctx.lineTo(cx, cy - outerRadius)
     ctx.closePath();
 
+    ctx.strokeStyle='blue';
+    ctx.stroke();
+
     ctx.fillStyle='#ffff66';
 
     ctx.fill();
+
+    ctx.restore();
+}
+
+function draw_star_outline(cx, cy, spikes, outerRadius, innerRadius) {
+    var rot = Math.PI / 2 * 3;
+    var x = cx;
+    var y = cy;
+    var step = Math.PI / spikes;
+
+    ctx.beginPath();
+    ctx.moveTo(cx, cy - outerRadius)
+    for (i = 0; i < spikes; i++) {
+        x = cx + Math.cos(rot) * outerRadius;
+        y = cy + Math.sin(rot) * outerRadius;
+        ctx.lineTo(x, y)
+        rot += step
+
+        x = cx + Math.cos(rot) * innerRadius;
+        y = cy + Math.sin(rot) * innerRadius;
+        ctx.lineTo(x, y)
+        rot += step
+    }
+    ctx.lineTo(cx, cy - outerRadius)
+    ctx.closePath();
+
+
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'black';
+    ctx.stroke();
+
+    ctx.restore();
 }
 
 function paint_star_canvas(canvas_id, rating) {
@@ -34,7 +69,7 @@ function paint_star_canvas(canvas_id, rating) {
     ctx.clearRect(0, 0, canvas.width, canvas.height); // only for testing purposes
 
     x_index = 13;
-    for(a=0; a < 5; a++) {
+    for(a=0; a < 6; a++) {
         draw_star(x_index, 14, 5, 14, 7);
 
         x_index += 26;
@@ -44,10 +79,16 @@ function paint_star_canvas(canvas_id, rating) {
         rect_pos = 25 * rating;
         ctx.rect(rect_pos, 0, canvas.width, canvas.height);
 
-        ctx.fillStyle = '#'+Math.random().toString(16).substr(-6);
+        ctx.fillStyle = "white";
         ctx.fill();
     }
-}
+
+    x_index = 13;
+    for(a=0; a < 6; a++) {
+        draw_star_outline(x_index, 14, 5, 14, 7);
+
+        x_index += 26;
+    }}
 
 function average_rating(weighted_rating, num_votes) {
     var a = 10;
